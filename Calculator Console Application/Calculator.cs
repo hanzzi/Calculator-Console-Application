2d6667d52463534933d868254648895ce8e22219
hans2734 <hans2734@LAP-1MTMMX1>
2017-01-11 14:42:41 +0100

    Added more Operators

--- Calculator Console Application/Calculator.cs
+++ Calculator Console Application/Calculator.cs
@@ -8,35 +8,11 @@ namespace Calculator_Console_Application
 {
     class Calculator
     {
-        public double Calculate(double Num1, string Operator, double Num2)
-        {
-            if (Operator == "-" || Operator == "+" || Operator == "/" || Operator == "*")
-            {
-                switch (Operator)
-                {
-                    case "-":
-                        return Num1 - Num2;
-
-                    case "+":
-                        return Num1 + Num2;
-
-                    case "/":
-                        return Num1 / Num2;
-
-                    case "*":
-                        return Num1 * Num2;
-                    default:
-                        Console.WriteLine("Error in Operator please check if you are using a correct operator");
-                        break;
-                }
-            }
-            return 0;
-        }
-
         public double Addition(double Num1, double Num2)
         {
             // Adds two parameters together
             double Value = Num1 + Num2;
+            Program.PreviousResult = Value;
 
             return Value;
         }
@@ -44,6 +20,23 @@ namespace Calculator_Console_Application
         public double Subtract(double Num1, double Num2)
         {
             double Value = Num1 - Num2;
+            Program.PreviousResult = Value;
+
+            return Value;
+        }
+
+        public double Multiply(double Num1, double Num2)
+        {
+            double Value = Num1 * Num2;
+            Program.PreviousResult = Value;
+
+            return Value;
+        }
+
+        public double Divide(double Num1, double Num2)
+        {
+            double Value = Num1 / Num2;
+            Program.PreviousResult = Value;
 
             return Value;
         }
--- Calculator Console Application/Program.cs
+++ Calculator Console Application/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculator_Console_Application
@@ -15,24 +16,31 @@ namespace Calculator_Console_Application
             // Gets the first number
             Console.WriteLine("Please insert your first number");
             string Input1 = Console.ReadLine();
-            double DInput1;
-            double.TryParse(Input1, out DInput1);
-            
-            
+            double DInput1 = 0;
+            if (Input1 != "ANS")
+                double.TryParse(Input1, out DInput1);
+
+            if (Input1 == "ANS")
+                DInput1 = Program.PreviousResult;
+
             // Gets the Operator           
-            Console.WriteLine("Please insert Operator");
+            Console.WriteLine("Please insert Operator (+, -, *, /, ^, sqrt, random)");
             string Operator = Console.ReadLine();
+            double DInput2 = 0;
 
-            // Gets the second Number
-            Console.WriteLine("Please Insert your second number");
-            string Input2 = Console.ReadLine();
-            double DInput2;
-            double.TryParse(Input2, out DInput2);
+            if (Operator != "sqrt")
+            {
+                // Gets the second Number
+                Console.WriteLine("Please Insert your second number");
+                string Input2 = Console.ReadLine();
+                if (Input2 != "ANS")
+                    double.TryParse(Input2, out DInput2);
+                if (Input2 == "ANS")
+                    DInput2 = PreviousResult;
+            }
 
-
-            // Opretter et nyt OBJEKT new Calculator == et OBJEKT
-            Calculator Calc = new Calculator();
-            double Result = Calc.Calculate(DInput1, Operator, DInput2);
+            Program Prog = new Program();
+            double Result = Prog.DetermineMethod(DInput1, Operator, DInput2);
 
             Console.WriteLine("Result");
             Console.WriteLine(Result);
@@ -40,7 +48,43 @@ namespace Calculator_Console_Application
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
             Main(null);
+        }
 
+        public static double PreviousResult;
+
+        public double DetermineMethod(double Num1, string Operator, double Num2)
+        {
+            // Opretter et nyt OBJEKT new Calculator == et OBJEKT
+            AdvancedCalculator Calc = new AdvancedCalculator();
+
+            if (Operator == "-" || Operator == "+" || Operator == "/" || Operator == "*" || Operator == "^" || Operator == "sqrt" || Operator == "random")
+            {
+                switch (Operator)
+                {
+                    case "-":
+                        return Calc.Subtract(Num1, Num2);
+
+                    case "+":
+                        return Calc.Addition(Num1, Num2);
+
+                    case "/":
+                        return Calc.Divide(Num1, Num2);
+
+                    case "*":
+                        return Calc.Multiply(Num1, Num2);
+                    case "^":
+                        return Calc.Power(Num1, Num2);
+                    case "sqrt":
+                        return Calc.SquareRoot(Num1);
+                    case "random":
+                        return Calc.Random(Num1, Num2);
+                    default:
+                        Console.WriteLine("Error in Operator please check if you are using a correct operator");
+                        break;
+                }
+            }
+            return 0;
         }
     }
 }
+
