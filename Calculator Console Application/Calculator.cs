12089607ddf4832a295681b68b1a28fe4e90daa3
hans2734 <hans2734@LAP-1MTMMX1>
2017-01-11 12:14:14 +0100

    Made a little Calculator

---
+++ Calculator Console Application/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Console_Application
+{
+    class Calculator
+    {
+        public double Calculate(double Num1, string Operator, double Num2)
+        {
+            if (Operator == "-" || Operator == "+" || Operator == "/" || Operator == "*")
+            {
+                switch (Operator)
+                {
+                    case "-":
+                        return Num1 - Num2;
+
+                    case "+":
+                        return Num1 + Num2;
+
+                    case "/":
+                        return Num1 / Num2;
+
+                    case "*":
+                        return Num1 * Num2;
+                    default:
+                        Console.WriteLine("Error in Operator please check if you are using a correct operator");
+                        break;
+                }
+            }
+            return 0;
+        }
+
+        public double Addition(double Num1, double Num2)
+        {
+            // Adds two parameters together
+            double Value = Num1 + Num2;
+
+            return Value;
+        }
+
+        public double Subtract(double Num1, double Num2)
+        {
+            double Value = Num1 - Num2;
+
+            return Value;
+        }
+
+
+    }
+}
--- Calculator Console Application/Program.cs
+++ Calculator Console Application/Program.cs
@@ -10,6 +10,37 @@ namespace Calculator_Console_Application
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+
+            // Gets the first number
+            Console.WriteLine("Please insert your first number");
+            string Input1 = Console.ReadLine();
+            double DInput1;
+            double.TryParse(Input1, out DInput1);
+            
+            
+            // Gets the Operator           
+            Console.WriteLine("Please insert Operator");
+            string Operator = Console.ReadLine();
+
+            // Gets the second Number
+            Console.WriteLine("Please Insert your second number");
+            string Input2 = Console.ReadLine();
+            double DInput2;
+            double.TryParse(Input2, out DInput2);
+
+
+            // Opretter et nyt OBJEKT new Calculator == et OBJEKT
+            Calculator Calc = new Calculator();
+            double Result = Calc.Calculate(DInput1, Operator, DInput2);
+
+            Console.WriteLine("Result");
+            Console.WriteLine(Result);
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Main(null);
+
         }
     }
 }
